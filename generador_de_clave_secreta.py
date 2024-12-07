259634877dc332f5932602ccc9e480dc038e66f4
IamRodion <22568039+IamRodion@users.noreply.github.com>
2024-12-06 21:33:01 -0500

    Modifica el archivo "generador_de_clave_secreta.py" para que guarde contraseñas automáticamente Añade líneas de código para que escriba un archivo .env con una nueva clave secreta en la misma ruta donde se ejecutó el generador de claves

--- generador_de_clave_secreta.py
+++ generador_de_clave_secreta.py
@@ -4,5 +4,7 @@ def generate_secret_key():
     return ''.join(secrets.choice('abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-_=+[]{}|;:,.<>/?') for i in range(50))
 
 # Generar una nueva clave y almacenarla en una variable
-secret_key = generate_secret_key()
-print(secret_key)
+SECRET_KEY = generate_secret_key()
+# print(secret_key)
+with open('.env', mode='w+') as file:
+    file.write("SECRET_KEY = '{}'".format(SECRET_KEY))
